d8c126e936d2a8da37bd598aeb8376ee9ce5754f
Steven D Vachon <contact@svachon.com>
2018-03-07 16:39:52 -0500

    v2.0.0-beta

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 > Checks whether a value is a WHATWG [`URL`](https://developer.mozilla.org/en/docs/Web/API/URL).
 
 
-Works cross-realm/iframe and despite @@toStringTag.
+Works cross-realm/iframe and despite `Symbol.toStringTag`.
 
 
 ## Installation
 
-[Node.js](http://nodejs.org/) `>= 4` is required. To install, type this at the command line:
+[Node.js](http://nodejs.org/) `>= 6` is required. To install, type this at the command line:
 ```shell
 npm install isurl
 ```
@@ -34,7 +34,7 @@ isURL.lenient(url);  //-> true
 
 
 [npm-image]: https://img.shields.io/npm/v/isurl.svg
-[npm-url]: https://npmjs.org/package/isurl
+[npm-url]: https://npmjs.com/package/isurl
 [travis-image]: https://img.shields.io/travis/stevenvachon/isurl.svg
 [travis-url]: https://travis-ci.org/stevenvachon/isurl
 [greenkeeper-image]: https://badges.greenkeeper.io/stevenvachon/isurl.svg
--- test.js
+++ test.js
@@ -1,50 +1,61 @@
 "use strict";
-const expect = require("chai").expect;
+const customizeURL = require("incomplete-url");
+const {describe, it} = require("mocha");
+const {expect} = require("chai");
 const isURL = require("./");
-const parseURL = require("url").parse;
-const semver = require("semver");
+const {parse: parseURL} = require("url");
+const {URL, URLSearchParams} = require("universal-url");
 
-const atLeastNode6 = semver.satisfies(process.version, ">= 6");
 
-const it_atLeastNode6 = atLeastNode6 ? it : it.skip;
 
-let URL,URLSearchParams;
-
-if (atLeastNode6)
+it("rejects non-URLs", () =>
 {
-	const uurl = require("universal-url");
-	URL = uurl.URL,
-    URLSearchParams = uurl.URLSearchParams;
-}
+	const fixtures =
+	[
+		"http://domain/",
+		parseURL("http://domain/"),
+		Symbol("http://domain/"),
+		{},
+		[],
+		/regex/,
+		true,
+		1,
+		null,
+		undefined
+	];
 
-
-
-it_atLeastNode6("accepts a full implemention", function()
-{
-	expect( isURL(new URL("http://domain/")) ).to.be.true;
-	expect( isURL(new URL("http://domain/"),false) ).to.be.true;
+	fixtures.forEach(fixture =>
+	{
+		expect( isURL(fixture) ).to.be.false;
+		expect( isURL.lenient(fixture) ).to.be.false;
+	});
 });
 
 
 
-it_atLeastNode6("can accept a partial implemention", function()
+it("rejects a mocked partial implementation using toStringTag", () =>
 {
-	function IncompleteURL(url, base)
-	{
-		this.url = new URL(url, base);
+	const mock = { href:"http://domain/" };
 
-		// Extend all `URL` getters except `searchParams`
-		Object.keys(URL.prototype)
-		.filter(key => key !== "searchParams")
-		.forEach(key => Object.defineProperty
-		(
-			this, key,
-			{
-				get: () => this.url[key],
-				set: newValue => this.url[key] = newValue
-			}
-		));
-	}
+	mock[Symbol.toStringTag] = "URL";
+
+	expect( isURL(mock) ).to.be.false;
+	expect( isURL.lenient(mock) ).to.be.false;
+});
+
+
+
+it("accepts a full implemention", () =>
+{
+	expect( isURL(new URL("http://domain/")) ).to.be.true;
+	expect( isURL.lenient(new URL("http://domain/")) ).to.be.true;
+});
+
+
+
+it("can accept a partial implemention", () =>
+{
+	const {IncompleteURL} = customizeURL({ noSearchParams:true });
 
 	expect( isURL(new IncompleteURL("http://domain/")) ).to.be.false;
 	expect( isURL.lenient(new IncompleteURL("http://domain/")) ).to.be.true;
@@ -52,37 +63,9 @@ it_atLeastNode6("can accept a partial implemention", function()
 
 
 
-it("rejects non-URLs", function()
+describe("Weaknesses", () =>
 {
-	expect( isURL("http://domain/") ).to.be.false;
-	expect( isURL(parseURL("http://domain/")) ).to.be.false;
-	expect( isURL(Symbol("http://domain/")) ).to.be.false;
-	expect( isURL({}) ).to.be.false;
-	expect( isURL([]) ).to.be.false;
-	expect( isURL(/regex/) ).to.be.false;
-	expect( isURL(true) ).to.be.false;
-	expect( isURL(1) ).to.be.false;
-	expect( isURL(null) ).to.be.false;
-	expect( isURL(undefined) ).to.be.false;
-	expect( isURL() ).to.be.false;
-});
-
-
-
-it("rejects a mocked partial implementation using toStringTag", function()
-{
-	const mock = { href:"http://domain/" };
-
-	mock[Symbol.toStringTag] = "URL";
-
-	expect( isURL(mock) ).to.be.false;
-});
-
-
-
-describe("Weaknesses", function()
-{
-	it("accepts a mocked full implementation", function()
+	it("accepts a mocked full implementation", () =>
 	{
 		const mock =
 		{
@@ -106,7 +89,7 @@ describe("Weaknesses", function()
 
 
 
-	it("can accept a mocked partial implementation", function()
+	it("can accept a mocked partial implementation", () =>
 	{
 		const mock =
 		{
